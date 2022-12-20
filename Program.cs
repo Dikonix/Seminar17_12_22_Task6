bfb2d1d284aafdeb6373ed77874661bb34b8ad43
Diana Brik <di.brik@yandex.kz>
2022-12-20 20:42:50 +0600

    Seminar 17.12.22 Task 6

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+int n;
+
+try
+{
+    n = Convert.ToInt32(Console.ReadLine());
+}
+catch (FormatException)
+{
+    Console.WriteLine("Введено не целое число");
+    return;
+}
+
+if (n % 2 == 0)
+{
+    Console.WriteLine($"{n} - четное число");
+}
+else
+{
+    Console.WriteLine($"{n} - нечетное число");
+}
